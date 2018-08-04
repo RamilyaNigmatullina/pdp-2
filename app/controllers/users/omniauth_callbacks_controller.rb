8fe498e346f5733b5a13799648c117db7714edab
--global <ramilya.nigmatullina@flatstack.com>
2018-08-04 22:46:01 +0300

    Change rubocop configs and fix quality

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,33 +1,31 @@
-module Users
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def facebook
-      bar
-    end
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    bar
+  end
 
-    def google_oauth2
-      bar
-    end
+  def google_oauth2
+    bar
+  end
 
-    def failure
-      redirect_to root_path
-    end
+  def failure
+    redirect_to root_path
+  end
 
-    private
+  private
 
-    def bar
-      if authenticate_user.success?
-        sign_in_and_redirect authenticate_user.user, event: :authentication
-      else
-        redirect_to root_path, alert: authenticate_user.message
-      end
+  def bar
+    if authenticate_user.success?
+      sign_in_and_redirect authenticate_user.user, event: :authentication
+    else
+      redirect_to root_path, alert: authenticate_user.message
     end
+  end
 
-    def authenticate_user
-      @authenticate_user ||= Users::Authenticate.call(auth_data: facebook_auth_data)
-    end
+  def authenticate_user
+    @authenticate_user ||= Users::Authenticate.call(auth_data: facebook_auth_data)
+  end
 
-    def authentication_hash
-      request.env["omniauth.auth"]
-    end
+  def authentication_hash
+    request.env["omniauth.auth"]
   end
 end
