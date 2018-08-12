88711186c1f2c48a5bf59ab15a242bfbc814f555
--global <ramilya.nigmatullina@flatstack.com>
2018-08-12 12:45:23 +0300

    Translate seeds into English

--- spec/features/hotels/index_spec.rb
+++ spec/features/hotels/index_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 feature "List Articles" do
   let!(:first_hotel) do
-    create :hotel, name: "Гостиничный комплекс Татарстан", address: "г. Казань, ул. Пушкина, 4",
+    create :hotel, name: "Hotel complex Tatarstan", address: "Pushkin str. 4, Kazan",
                    longitude: 49.12240500000007, latitude: 55.786463, rating: 7.4, stars: 3
   end
   let!(:second_hotel) do
-    create :hotel, name: "Отель НОГАЙ", address: "г. Казань, ул. Профсоюзная улица, 16Б",
+    create :hotel, name: "Hotel Nogai", address: "Profsoyuznaya str. 16B, Kazan",
                    longitude: 49.11462860000006, latitude: 55.7919311, rating: 9.1, stars: 3
   end
 
@@ -21,16 +21,16 @@ feature "List Articles" do
     expect(page).to have_selector(hotel_selector, count: 2)
 
     within first_hotel_selector do
-      expect(page).to have_content("Гостиничный комплекс Татарстан")
-      expect(page).to have_content("Address: г. Казань, ул. Пушкина, 4")
+      expect(page).to have_content("Hotel complex Tatarstan")
+      expect(page).to have_content("Address: Pushkin str. 4, Kazan")
       expect(page).to have_content("Stars: 3")
       expect(page).to have_content("Rating: 7.4")
       expect(page).to have_content("Distance: 3 км.")
     end
 
     within second_hotel_selector do
-      expect(page).to have_content("Отель НОГАЙ")
-      expect(page).to have_content("Address: г. Казань, ул. Профсоюзная улица, 16Б")
+      expect(page).to have_content("Hotel Nogai")
+      expect(page).to have_content("Address: Profsoyuznaya str. 16B, Kazan")
       expect(page).to have_content("Stars: 3")
       expect(page).to have_content("Rating: 9.1")
       expect(page).to have_content("Distance: 3 км.")
@@ -40,7 +40,7 @@ feature "List Articles" do
   scenario "User searches hotels" do
     visit hotels_path
 
-    fill_in "Search", with: "Гостиничный комплекс Татарстан"
+    fill_in "Search", with: "Hotel complex Tatarstan"
     fill_in "Min Rating", with: "7"
     fill_in "Max Rating", with: "8"
     fill_in "Stars Amount", with: "3"
@@ -50,8 +50,8 @@ feature "List Articles" do
     expect(page).to have_selector(hotel_selector, count: 1)
 
     within first_hotel_selector do
-      expect(page).to have_content("Гостиничный комплекс Татарстан")
-      expect(page).to have_content("Address: г. Казань, ул. Пушкина, 4")
+      expect(page).to have_content("Hotel complex Tatarstan")
+      expect(page).to have_content("Address: Pushkin str. 4, Kazan")
       expect(page).to have_content("Stars: 3")
       expect(page).to have_content("Rating: 7.4")
       expect(page).to have_content("Distance: 3 км.")
