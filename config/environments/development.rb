f79b9f0067a01ac544662fc2c59373af945b1ab0
--global <ramilya.nigmatullina@flatstack.com>
2018-08-17 23:12:56 +0300

    Correct config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,6 @@ end
 
 class ActionDispatch::Request
   def location
-    Geocoder.search("72.229.28.185").first # America
-    # Geocoder.search("91.107.102.152").first
+    Geocoder.search("91.107.102.152").first
   end
 end
