87cd98cd860cec7b0e8f857bb55b216e23a10165
--global <ramilya.nigmatullina@flatstack.com>
2018-08-25 14:29:02 +0300

    Try to fix spec in semaphore

--- spec/support/shared_context/time_is_frozen.rb
+++ spec/support/shared_context/time_is_frozen.rb
@@ -1,5 +1,5 @@
 shared_context :time_is_frozen do
-  let(:current_time) { Time.new(2018, 8, 14, 14, 0, 0).in_time_zone("Moscow") }
+  let(:current_time) { Time.new(2018, 8, 14, 14, 0, 0, "+03:00").in_time_zone("Moscow") }
 
   before { Timecop.freeze(current_time) }
 
