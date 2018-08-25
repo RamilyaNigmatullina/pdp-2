7d44849184c2cb013f3bb927d4239bcaed7e6640
--global <ramilya.nigmatullina@flatstack.com>
2018-08-25 19:31:37 +0300

    Code improvments

--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -2,7 +2,4 @@ class SearchForm
   include ActiveModel::Model
 
   attr_accessor :search, :stars, :min_rating, :max_rating, :radius, :coordinates
-
-  validates :stars_amount, numericality: { only_integer: true, allow_blank: true }
-  validates :min_rating, :max_rating, :radius, numericality: { allow_blank: true }
 end
